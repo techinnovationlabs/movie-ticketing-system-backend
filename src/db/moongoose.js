0ec83aacdc42df136847334311f790844366e400
vignesh-asahi <vignesh@asahitechnologies.com>
2021-10-25 15:34:32 +0530

    initial code setup with basic dependencies.

---
+++ src/db/moongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/movie-ticketing-system', {
+    useNewUrlParser: true
+});
+
+mongoose.connection.on("open", function (ref) {
+    console.log("Connected to mongo server.");
+
+});
+
+mongoose.connection.on("error", function (err) {
+    console.log("Could not connect to mongo server!");
+});
