ae1256a0da656b356147380e5e26792fbb8b1a2d
vignesh-asahi <vignesh@asahitechnologies.com>
2021-10-27 22:27:30 +0530

    implemented the booking flow

--- src/controllers/show.js
+++ src/controllers/show.js
@@ -3,13 +3,24 @@ const Screen = require("../models/screen");
 
 const fetchShows = async (req, res) => {
     try {
-        const shows = await Show.find().populate("movie").populate("screen").populate("showTime");
+        let shows;
+        if (req.query.movieId && req.query.date) {
+            shows = await Show.find({
+                movie: req.query.movieId, date: {
+                    $gte: new Date(new Date(req.query.date).setHours(00, 00, 00)),
+                    $lt: new Date(new Date(req.query.date).setHours(23, 59, 59))
+                }
+            }).populate("movie").populate("screen").populate("showTime");
+        } else {
+            shows = await Show.find().populate("movie").populate("screen").populate("showTime");
+        }
         res.send(shows);
     } catch (e) {
         res.status(500)
             .send("An error occurred while fetching the shows: " + e.message);
     }
 };
+
 
 const createShow = async (req, res) => {
     const { screenId, slotId, date, movieId } = req.body;
@@ -21,10 +32,12 @@ const createShow = async (req, res) => {
         const availability = screen.tier.map(tier => {
             let availableSeats = [];
             const tierCode = tier.code;
+            const name = tier.name;
+            const price = tier.price;
             tier.rows.map(row => {
                 availableSeats = availableSeats.concat(getSeats(row));
             });
-            return { availableSeats, tierCode };
+            return { availableSeats, tierCode, name, price };
         });
         const show = new Show({ date: date, availability, movie: movieId, showTime: slotId, screen });
         await show.save();
@@ -36,7 +49,7 @@ const createShow = async (req, res) => {
 
 const getSeats = (row, seatsPerRow = 10) => {
     const seats = [];
-    for (let i = 1; i <= seatsPerRow; i++) {
+    for (let i = 1; i <= seatsPerRow; i = i + 2) {
         seats.push(`${row}${i}`);
     }
     return seats;
@@ -45,4 +58,5 @@ const getSeats = (row, seatsPerRow = 10) => {
 module.exports = {
     fetchShows,
     createShow,
+    fetchShowByMovieAndDate
 };
